b731bf2dbf5e0d73617d08d505669eaacd6583b7
Julia Francais <octave_julia_francais@MacBook-Pro-de-Julia.local>
2020-11-04 16:21:09 +0100

    add authent (login)

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -1,12 +1,69 @@
-import { RouterContext } from "../deps.ts";
+import { RouterContext, hashSync, compareSync, create } from "../deps.ts";
+import User from "../models/User.ts";
+import { usersCollection } from "../mongo.ts";
+
+// const payload = await verify(jwt, "secret", "HS512"); // { foo: "bar" }
 
 class AuthController {
-  login() {}
+  async login(ctx: RouterContext) {
+    const result = ctx.request.body();
+    if (result.type === "json") {
+      const { email, password } = await result.value;
+      if (!email || !password) {
+        ctx.response.status = 422;
+        ctx.response.body = { message: "Please provide email and password" };
+        return;
+      }
+      let _user: any = await User.findOne({ email });
+      if (!_user) {
+        ctx.response.status = 422;
+        ctx.response.body = { message: "User doesn't exist" };
+        return;
+      }
+      console.log("_user", _user);
+      if (!compareSync(password, _user.password)) {
+        ctx.response.status = 422;
+        ctx.response.body = { message: "Incorrect password" };
+        return;
+      }
+
+      const jwt = await create(
+        { alg: "HS512", typ: "JWT" },
+        {
+          iss: _user.email,
+          exp: Date.now() / 1000 + 60 * 60,
+        },
+        "secret"
+      );
+
+      ctx.response.body = {
+        id: _user.id,
+        name: _user.name,
+        email: _user.email,
+      };
+    }
+  }
+
   async register(ctx: RouterContext) {
     const result = ctx.request.body();
     if (result.type === "json") {
       const { name, email, password } = await result.value;
-      console.log(name, email, password);
+
+      let _user = await User.findOne({ email });
+      if (_user) {
+        ctx.response.status = 422;
+        ctx.response.body = { message: "Email is already used" };
+        return;
+      }
+      const hashedPassword = hashSync(password);
+      let user = new User({ name, email, password: hashedPassword });
+      await user.save();
+      ctx.response.status = 201;
+      ctx.response.body = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      };
     }
   }
 }
--- models/User.ts
+++ models/User.ts
@@ -1,1 +1,27 @@
-export default class User {}
+import { usersCollection } from "../mongo.ts";
+
+export default class User {
+  public id: string;
+  public name: string;
+  public email: string;
+  public password: string;
+
+  constructor({ id = "", name = "", email = "", password = "" }) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+  static findOne(params: object) {
+    console.log(params);
+    return usersCollection.findOne(params);
+  }
+
+  async save() {
+    delete this.id;
+    const { $oid } = await usersCollection.insertOne(this);
+    console.log($oid);
+    this.id = $oid;
+    return this;
+  }
+}
